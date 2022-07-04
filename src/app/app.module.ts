392b4ad9858b60de1c1ba1ceda3c63df2d8105aa
sasmisahu <sasmitasahu.se@gmail.com>
2022-07-04 14:24:18 +0530

    first commit with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { PromotionComponent } from './promotion/promotion.component';
+import { CartComponent } from './cart/cart.component';
+import { OffersComponent } from './offers/offers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent,
+    PromotionComponent,
+    CartComponent,
+    OffersComponent
   ],
   imports: [
     BrowserModule,
